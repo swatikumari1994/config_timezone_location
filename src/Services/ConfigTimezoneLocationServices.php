04c61a954b98b7ae7f4e1b0398bdc0c7319b0464
swatikumari1994 <swatiecekumari1994@gmail.com>
2022-12-12 03:52:10 +0530

    Drupal Coding Standard for Services

--- src/Services/ConfigTimezoneLocationServices.php
+++ src/Services/ConfigTimezoneLocationServices.php
@@ -1,8 +1,15 @@
 <?php
 
 /**
- * @file providing the service set config form.
- *
+ * Config Timezone.
+ * 
+ * @file      providing the service set config form.
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    swati <swatiecekumari1994@gmail.com>
+ * @copyright 2022 drupal
+ * @license   http://localhost/drupal PHP License 7.0
+ * @link      http://localhost/drupal/admin/config/config-timezone-location/settings
  */
 
 namespace Drupal\config_timezone_location\Services;
@@ -10,56 +17,80 @@ namespace Drupal\config_timezone_location\Services;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Datetime\DateFormatter;
 use Drupal\Core\Datetime\DrupalDateTime;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Class Config Timezone Location Services
- * @package Drupal\config_timezone_location\Services
+ *
+ * @category  PHP
+ * @package   Drupalconfig_Timezone_LocationServices
+ * @author    swati <swatiecekumari1994@gmail.com>
+ * @copyright 2022 drupal
+ * @license   http://localhost/drupal PHP License 7.0
+ * @link      http://localhost/drupal/admin/config/config-timezone-location/settings
  */
-class ConfigTimezoneLocationServices {
-
-  /**
-   * The config factory.
-   *
-   * @var \Drupal\Core\Config\ConfigFactoryInterface
-   */
-  protected $configFactory;
-
-  /**
-   * The date formatter service.
-   *
-   * @var \Drupal\Core\Datetime\DateFormatter
-   */
-  protected $dateFormatter;
-
-  /**
-   * Class constructor.
-   * @param \Drupal\Core\Config\ConfigFactoryInterface $configFactory
-   *   The configuration factory.
-   * @param \Drupal\Core\Datetime\DateFormatter $dateFormatter
-   *   The date formatter service.
-   */
-  public function __construct(ConfigFactoryInterface $configFactory , DateFormatter $dateFormatter){
-    $this->configFactory = $configFactory;
-    $this->dateFormatter = $dateFormatter;
-  }
+class ConfigTimezoneLocationServices
+{
 
     /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container) {
-    return new static(
-      $container->get('config.factory'),
-      $container->get('date.formatter')
-    );
-  }
+     * The config factory.
+     *
+     * @var \Drupal\Core\Config\ConfigFactoryInterface
+     */
+    protected $configFactory;
+
+    /**
+     * The date formatter service.
+     *
+     * @var \Drupal\Core\Datetime\DateFormatter
+     */
+    protected $dateFormatter;
+
+    /**
+     * Class constructor.
+     *
+     * @param \Drupal\Core\Config\ConfigFactoryInterface $configFactory
+     *   The configuration factory.
+     * @param \Drupal\Core\Datetime\DateFormatter        $dateFormatter
+     *   The date formatter service.
+     */
+    public function __construct(ConfigFactoryInterface $configFactory , 
+        DateFormatter $dateFormatter
+    ) {
+        $this->configFactory = $configFactory;
+        $this->dateFormatter = $dateFormatter;
+    }
+
+    /**
+     *  Class Container.
+     * 
+     * @param Symfony\Component\DependencyInjection\ContainerInterface $container
+     * Container.
+     * 
+     * @return object
+     */
+    public static function create(ContainerInterface $container)
+    {
+        return new static(
+            $container->get('config.factory'),
+            $container->get('date.formatter')
+        );
+    }
   
-  /**
-   * {@inheritdoc}
-   */
-  public function getData() {
-    $config = $this->configFactory->get('config_timezone_location.settings');
-    $timezone = $config->get('timezone');
-    $date = $this->dateFormatter->format(strtotime('now'), 'custom' , 'jS M Y - h:i A' , $timezone);
-    return $date;
-  }
+    /**
+     * Returns date.
+     *
+     * @return string $date
+     *   The date.
+     */
+    public function getData()
+    {
+        $config = $this->configFactory->get('config_timezone_location.settings');
+        $timezone = $config->get('timezone');
+        $date = $this->dateFormatter->format(
+            strtotime('now'), 
+            'custom', 'jS M Y - h:i A', $timezone
+        );
+        return $date;
+    }
 }
